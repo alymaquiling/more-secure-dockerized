a0114fa2e08e7bb83ef5eddbfd6f4b27d650ba55
Alyssa Maquiling <alymaquiling@gmail.com>
2021-01-17 17:41:04 -0500

    login page error message updated

--- server/app/controllers/auth.controller.js
+++ server/app/controllers/auth.controller.js
@@ -35,7 +35,7 @@ exports.signin = (req, res) => {
   })
     .then(user => {
       if (!user) {
-        return res.status(404).send({ message: 'User Not found.' });
+        return res.status(404).send({ message: 'Invalid username or password!' });
       }
 
       var passwordIsValid = bcrypt.compareSync(
@@ -46,7 +46,7 @@ exports.signin = (req, res) => {
       if (!passwordIsValid) {
         return res.status(401).send({
           accessToken: null,
-          message: 'Invalid Password!'
+          message: 'Invalid username or password!'
         });
       }
 
